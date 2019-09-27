ca19ad46f647ff74e0afc375019016d817ebc917
Rémi de Verclos <r.deverclos@math.ru.nl>
2019-09-27 12:40:47 +0200

    Updated the examples Removed the 'crate' dependency

--- examples/goodman.rs
+++ examples/goodman.rs
@@ -1,23 +1,22 @@
 extern crate flag_algebra;
 
 use flag_algebra::*;
-use crate::sdp::Problem;
-use crate::flags::Graph;
-use crate::operator::Basis;
+use flags::Graph;
+use operator::Basis;
+use sdp::Problem;
 
 pub fn main() {
     // Work on the graphs of size 3.
     let basis = Basis::new(3);
 
     // Define useful flags.
-    let k3 = flag(&Graph::new(3, &[(0,1),(1,2),(2,0)])); // Triangle
+    let k3 = flag(&Graph::new(3, &[(0, 1), (1, 2), (2, 0)])); // Triangle
     let e3 = flag(&Graph::new(3, &[])); // Independent set of size 3
 
     // Definition of the optimization problem.
     let pb = Problem::<i64, _> {
         // Constraints
-        ineqs: vec!(total_sum_is_one(basis),
-                    flags_are_nonnegative(basis)),
+        ineqs: vec![total_sum_is_one(basis), flags_are_nonnegative(basis)],
         // Use all relevant Cauchy-Schwarz inequalities.
         cs: basis.all_cs(),
         // Minimize density of triangle plus density of independent of size 3.
--- examples/turan.rs
+++ examples/turan.rs
@@ -1,26 +1,26 @@
 extern crate flag_algebra;
 
 use flag_algebra::*;
-use crate::sdp::Problem;
-use crate::flags::Graph;
-use crate::operator::Basis;
+use flags::Graph;
+use operator::Basis;
+use sdp::Problem;
 
 pub fn main() {
     // Work on the graphs of size 3.
     let basis = Basis::new(3);
 
     // Define useful flags.
-    let triangle = flag(&Graph::new(3, &[(0,1),(1,2),(2,0)]));
-    let edge = flag(&Graph::new(2, &[(0,1)]));
+    let triangle = flag(&Graph::new(3, &[(0, 1), (1, 2), (2, 0)]));
+    let edge = flag(&Graph::new(2, &[(0, 1)]));
 
     // Definition of the optimization problem.
     let pb = Problem::<i64, _> {
         // Constraints
-        ineqs: vec!(
+        ineqs: vec![
             total_sum_is_one(basis),
             flags_are_nonnegative(basis),
-            triangle.at_most(0)
-        ),
+            triangle.at_most(0),
+        ],
         // Use all relevant Cauchy-Schwarz inequalities.
         cs: basis.all_cs(),
         // Minimize minus density of edges expressed in the basis of flags of
