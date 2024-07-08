dd3fba3d6ab66f36dbeb08c3339f3ba7ced64a56
Rémi de Verclos <remi.deverclos@albelli.com>
2024-07-08 14:12:48 +0200

    split info fetching and header writing

--- src/problem/info.rs
+++ src/problem/info.rs
@@ -1,1 +1,17 @@
+use crate::algebra::IneqMeta;
+use crate::expr::Expr;
+use crate::operator::SplitCount;
+use crate::{Basis, Flag};
 
+pub struct ProblemInfo<N, F: Flag> {
+    pub flag_name: &'static str,
+    pub basis: Basis<F>,
+    pub n_flags: usize,
+    pub n_selected_ineqs: (usize, usize),
+    pub n_selected_cs: (usize, usize),
+    pub n_cs_subspace: usize,
+    pub obj_expr: Expr<N, F>,
+    pub obj_scale: u64,
+    pub ineq_group_infos: Vec<(IneqMeta<N, F>, (usize, usize))>,
+    pub cs: Vec<SplitCount<F>>,
+}
